2ad59d78545b70201d6cbb42e79444320525a372
oncole123 <oncole123@gmail.com>
2014-09-04 20:09:33 +0800

    Add Metro-UI theme

---
+++ application/views/upload_view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CI Jquery Multiple Upload</title>
+	<link rel="stylesheet" href="<?base_url();?>assets/css/metro-bootstrap.css">
+</head>
+<body class="metro">
+	<div class="container text-center">
+		<div class="main-content clearfix">
+			<h3>Multiple Upload With CodeIgniter And JQuery</h3>
+			<div class="grid">
+				<div class="row">
+					<div class="span5">
+						<button>
+							<input type="file" name="uploadFile">
+							<i class="icon-folder-2 on-right">Open</i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+	<script src="<?=base_url();?>assets/js/jquery.min.js"></script>
+	<script src="<?=base_url();?>assets/js/jquery.widget.min.js"></script>
+	<script src="<?=base_url();?>assets/js/metro.min.js"></script>
+</html>
